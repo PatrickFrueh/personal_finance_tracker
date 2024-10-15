aef879444faa8888967b5ff9d7a0775fbeb0b13e
Patrick F. <patrick.frueh@gmx.net>
2024-10-15 20:24:13 +0200

    Read .csv-file

--- transform/transform.py
+++ transform/transform.py
@@ -1,5 +1,13 @@
-# load output file
+# import os
+# # print (os.getcwd())
 
-# remove delimiter
+# load output file
+import csv
+with open("./sample_output.csv", "r") as f:
+    reader = csv.reader(f, delimiter="\t")
+    for i, line in enumerate(reader):
+        print("line[{}] = {}".format(i, line))
+
+# cluster table in proper format
 
 # assign expense to corresponding category
