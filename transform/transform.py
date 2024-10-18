11d487dfc317af0be7d589eedbedb8236c1b2082
Patrick F. <patrick.frueh@gmx.net>
2024-10-18 18:08:17 +0200

    Start creating DataFrame for the read data

--- transform/transform.py
+++ transform/transform.py
@@ -1,17 +1,22 @@
-# os
-import os
-print(os.getcwd())
-
-# load extracted output file
 import csv
+
+# Initialization of DataFrame variables
+rows = []
+
+# @@@ create pandas frame and add items except for the first time
+# Load extracted output file
 with open("/home/pafr/repos/personal_finance_tracker/transform/sample_output.csv", "r") as f:
     reader = csv.reader(f, delimiter="|")
-    for i, line_list in enumerate(reader):
-        removed_space_list = [line.strip() for line in line_list]
-        print(removed_space_list)
+    for current_line_list in reader:
+        # removing 
+        stripped_line = [line.strip() for line in current_line_list]
+        rows.append()
 
-# cluster table into proper format
+# ---> @@@ : create DataFrame at beginning and instantly append to the frame isntead of making more lists
+        
+
+# @ cluster table into proper format
 # -> Remove first 2 lines
 # -> Delimiter: Pipe
 
-# assign expense to corresponding category
+# @ assign expense to corresponding category
