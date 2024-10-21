bbb0e974f40c3533cd0138b2df5a079ed7150a41
Patrick F. <patrick.frueh@gmx.net>
2024-10-21 18:20:07 +0200

    Add categorization using .json-file

--- transform/transform.py
+++ transform/transform.py
@@ -1,6 +1,8 @@
 import csv
 import pandas as pd
+import json
 
+# # Read bank logs
 # Define the DataFrame column names
 columns = ['Datum', 'Transaktionstyp', 'Begünstigter/Zahlungspflichtiger', 'IBAN', 'Betrag', 'Währung', 'Verwendungszweck', 'Kontostand nach Buchung']
 
@@ -23,11 +25,36 @@ with open("/home/pafr/repos/personal_finance_tracker/transform/sample_output.csv
         df.loc[len(df)] = stripped_line  # Appends row to the DataFrame
 
 
-# @@@ assign expense to corresponding category
+print(df)
 
-# check for keywords
-# -> assign relevant keywords: use an external file to keep it maintable, even for non-coding people
-# yaml? easy to read, first in contact with file type
+# # Categorize transactions
+# Load the keywords from the JSON file
+with open("/home/pafr/repos/personal_finance_tracker/transform/categories.json", "r") as file:
+    categories = json.load(file)
 
-# -> flag keywords that aren't assigned and need to be added
-# ??? later on: add ai categorization
+# Separate the description and recipient keyword lists
+description_keywords = categories['beschreibung_schluesselwoerter']
+recipient_keywords = categories['empfaenger_schluesselwoerter']
+
+def categorize_transaction(row):
+    description = row['Verwendungszweck'].lower()  # Convert the description to lowercase for comparison
+    recipient = row['Begünstigter/Zahlungspflichtiger'].lower()  # Convert the recipient to lowercase for comparison
+
+    # Description-based categorization
+    for category, keywords in description_keywords.items():
+        # Check if any keyword from the description_keywords list is in the 'Verwendungszweck' field
+        if any(keyword in description for keyword in keywords): # 1st: for keyword in keywords -> keyword in description
+            return category  # Return the category if a match is found
+
+    # Recipient-based categorization
+    for category, keywords in recipient_keywords.items():
+        # Check if any keyword from the recipient_keywords list is in the 'Empfänger' field
+        if any(keyword in recipient for keyword in keywords):
+            return category  # Return the category if a match is found
+
+    # If no category is matched, return 'Unkategorisiert'
+    return 'Unkategorisiert'
+
+# Apply the *new* categorization function to each row of the DataFrame
+df['Kategorie'] = df.apply(categorize_transaction, axis=1)
+
