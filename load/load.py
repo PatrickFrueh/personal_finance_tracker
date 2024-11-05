8227b2fc6bf2d4b2182848590386d386697c3d7d
Patrick F. <patrick.frueh@gmx.net>
2024-11-05 17:16:50 +0100

    Connect to mySql DB

--- load/load.py
+++ load/load.py
@@ -1,8 +1,26 @@
-# @@@ install mysql fedora
-# @@@ install python package
+import mysql.connector
+
+# Database configuration
+config = {
+    'user': 'patrick',
+    'password': 'Bazinga27@',
+    'host': 'localhost',
+    'database': 'expenses_database'
+}
+
+# Connect to the database
+try:
+    connection = mysql.connector.connect(**config)
+    if connection.is_connected():
+        print("Connected to MySQL database")
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
 
 # @ Import dataframe (result of transform process)
+# @@@ import transform.py, but enter path of bank data + categories.json as arguments
+# -> Result will be a dataframe (categorized + adjusted)
 
 # @ Write df to MySQL DB
 
-# @ Upload to online MySQL DB
+# @ Create/Upload to online MySQL DB
