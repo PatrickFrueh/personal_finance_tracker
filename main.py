caaff95e0c2bd4c5c07ffee14ccc1cf3b46937dc
Patrick F. <patrick.frueh@gmx.net>
2024-12-28 19:17:12 +0100

    Set structure for main file

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from src import transform
 
 def main():
+    # 1 - Extract: 
 
-    # 2 - Transform the extracted data using categories
+    # 2 - Transform: Transform the extracted data using categories
     categorized_data = transform.categorize_bank_transactions(
     bank_data_filepath="/home/pafr/repos/personal_finance_tracker/samples/sample_bank_data.csv",
     categories_filepath="/home/pafr/repos/personal_finance_tracker/.config/categories.json"
 )
-    print(categorized_data)
-
 
 
 if __name__ == "__main__":
