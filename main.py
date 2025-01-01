903a30917110afa3be742cfe1dc5c058b260c450
Patrick F. <patrick.frueh@gmx.net>
2025-01-01 23:32:04 +0100

    Add comments and structure

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src import transform
 def main():
     # 1 - Extract: 
 
-    # 2 - Transform: Transform the extracted data using categories
+    # 2 - Transform: Transform the extracted data using categories (as DataFrame)
     categorized_data = transform.categorize_bank_transactions(
     bank_data_filepath="/home/pafr/repos/personal_finance_tracker/samples/sample_bank_data.csv",
     categories_filepath="/home/pafr/repos/personal_finance_tracker/.config/categories.json"
