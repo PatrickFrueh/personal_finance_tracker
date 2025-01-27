dad0b2159123acd55e88472f8c68bb21595e42b0
Patrick F. <patrick.frueh@gmx.net>
2025-01-27 20:29:44 +0100

    Add multithreading to start up mock server

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import os
 from dotenv import load_dotenv
+import threading
 
+from tools.flask_server import app as flask_app
+from src import extract
 from src import transform
 from src import load
 
+# Temporary mock function: To be replaced with the actual API implementation in the future
+def run_flask_app():
+    """
+    Start the Flask app to access the mock API.
+    """
+    flask_app.run(port=5000)
+
 def main():
-    # @@@ 1 - Extract: 
+    # 1 - Extract:
+    # Start the Flask server in a separate thread
+    flask_thread = threading.Thread(target=run_flask_app)
+    flask_thread.daemon = True  # Exit even if the Flask server is still running
+    flask_thread.start()
+
+    # Download the CSV file from the mock API
+    API_URL = "http://127.0.0.1:5000/download-transactions"  # Mock API URL
+    csv_file_path = "res/downloaded_transactions.csv"  # Where to save the downloaded file
+    extract.download_csv_file(save_path=csv_file_path)
 
     # 2 - Transform
     # Transform the extracted data using categories (as DataFrame)
